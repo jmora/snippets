d948b1001f371dd435f944e699724dbbf1689345
Jose Mora <jmora@live.com>
2013-02-25 19:38:09 +0100

    Added testcase for unicornPatter
    From the weekend, commiting now.

--- java/unicornPatter/snippets.unicornPatter/src/test/java/com/github/jmora/snippets/unicornPatter/UnicornTest.java
+++ java/unicornPatter/snippets.unicornPatter/src/test/java/com/github/jmora/snippets/unicornPatter/UnicornTest.java
@@ -1,5 +1,6 @@
 package com.github.jmora.snippets.unicornPatter;
 
+import java.util.ArrayList;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -7,6 +8,18 @@ import org.junit.Test;
 public class UnicornTest extends TestCase {
 	@Test
 	public void testUnicorn() {
-		TestCase.assertTrue(true);
+		int i;
+		ArrayList<Boolean> res = new ArrayList<Boolean>();
+		for (i = 0; i < 15; i++)
+			res.add(Unicorn.pat());
+		TestCase.assertTrue(res.get(0));
+		for (i = 1; i < res.size(); i++)
+			TestCase.assertFalse(res.get(i));
+		res = new ArrayList<Boolean>();
+		for (i = 0; i < 25; i++)
+			res.add(Unicorn.pat());
+		TestCase.assertTrue(res.get(0));
+		for (i = 1; i < res.size(); i++)
+			TestCase.assertFalse(res.get(i));
 	}
 }
